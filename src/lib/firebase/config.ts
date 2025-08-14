0176312acc33423f5b7b1b3f5e01e3a63e740035
Muhammad Kabiru Rabiu <muhammadkabirmk52@gmail.com>
2025-08-14 15:35:41 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,17 +1,15 @@
-
 // src/lib/firebase/config.ts
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUf7xm5YVE2acm4N2AEy7AWoV4N8qEjuk",
-  authDomain: "kasupda-backend.firebaseapp.com",
-  projectId: "kasupda-backend",
-  storageBucket: "kasupda-backend.firebasestorage.app",
-  messagingSenderId: "185975290310",
-  appId: "1:185975290310:web:92cf858c4d7c53263e1ac7",
-  measurementId: "G-ZL76CBMYP1"
+  apiKey: "AIzaSyC9jYSTYDUzm7mbBZRK1J3UAIN94rgsYoQ",
+  authDomain: "kasupdaportal.firebaseapp.com",
+  projectId: "kasupdaportal",
+  storageBucket: "kasupdaportal.firebasestorage.app",
+  messagingSenderId: "1094508927078",
+  appId: "1:1094508927078:web:cc11d0332dac8550fcb5a7"
 };
 
 // Initialize Firebase
