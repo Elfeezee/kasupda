fe3bb27d1f770d9730146f772b6faf87e5eda796
Muhammad Kabiru Rabiu <muhammadkabirmk52@gmail.com>
2025-08-14 11:46:42 +0000

    i cant still see the application i supblmitted in the admin dashboard

--- src/app/admin/applications/page.tsx
+++ src/app/admin/applications/page.tsx
@@ -58,7 +58,6 @@ export default function ManageApplicationsPage() {
     // This code runs only on the client, after the component mounts
     const loadedApplications = getApplications();
     setApplications(loadedApplications);
-    setFilteredApplications(loadedApplications);
   }, []);
 
   useEffect(() => {
